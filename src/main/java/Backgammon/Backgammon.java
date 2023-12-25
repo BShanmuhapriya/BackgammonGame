f98b3d3e2fe34dfaee5dce5f51a7e8b716510b9d
Shanmuhapriya <68014808+Shanmuhapriya@users.noreply.github.com>
2023-12-25 00:20:22 +0000

    Sprint - 3: Backgammon.java initial code

--- src/main/java/Backgammon/Backgammon.java
+++ src/main/java/Backgammon/Backgammon.java
@@ -1,16 +1,24 @@
 package Backgammon;
 
+import java.util.Random;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Backgammon {
+    private static final int MATCH_LENGTH_DEFAULT = 5;
+    private static final int STAKE = 1;
+    private static final int NUM_DICE_SIDES = 6;
+
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         Message message = new Message();
-        message.diplayMessage();
 
-        int[] boardState =new int[24];
-        int currentPlayer = 1;
-        Board board = new Board();
-        board.displayBoard(boardState, currentPlayer);
-
-        Command cmd = new Command();
-        cmd.cmdInput();
+        String playerOneName = message.getPlayerName(1, scanner);
+        String playerTwoName = message.getPlayerName(2, scanner);
     }
+
 }
